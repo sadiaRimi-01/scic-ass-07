6a6cfe0d0c76d67b54c5b598eeee18d083fed465
sadiaRimi-01 <sadiaakter8138@gmail.com>
2026-01-08 18:14:45 +0600

    problem3

---
+++ problem3.js
@@ -0,0 +1,12 @@
+function isPalindrome(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+
+  return str === reversed;
+}
+
+console.log(isPalindrome("madam")); 
+console.log(isPalindrome("hello")); 
